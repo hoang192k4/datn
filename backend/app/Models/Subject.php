0aa1a37c53bf254aca73d934a797a2d8258cbafa
DOng <khanhdong1702pro@gmail.com>
2025-06-06 14:04:19 +0700

    update

--- backend/app/Models/Subject.php
+++ backend/app/Models/Subject.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Enums\Enums\Subject\SubjectStatus;
+use App\Enums\Subject\SubjectStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
