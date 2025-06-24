55af5e2f9fb03f7fe2afe4a021720bf4fdc6cb27
DOng <khanhdong1702pro@gmail.com>
2025-06-24 09:57:23 +0700

    fix tính điểm

--- backend/app/Services/SummaryGrade/SummaryGradeService.php
+++ backend/app/Services/SummaryGrade/SummaryGradeService.php
@@ -3,11 +3,13 @@
 namespace App\Services\SummaryGrade;
 
 use App\Enums\Evaluation;
+use App\Enums\Student\StudentStatus;
 use App\Enums\SummaryGrade\SummaryGradeType;
 use App\Enums\SummaryGrade\SummayryGradeEvaluation;
 use Exception;
 use App\Exceptions\ModelNotFoundByIdException;
 use App\Models\SummaryGrade;
+use App\Repositories\CourseSection\CourseSectionRepositoryInterface;
 use App\Services\Calculate\CalculateServiceInterface;
 use App\Repositories\Student\StudentRepositoryInterface;
 use App\Repositories\SummaryGrade\SummaryGradeRepositoryInterface;
@@ -22,15 +24,18 @@ class SummaryGradeService implements SummaryGradeServiceInterface
     protected $studentRepository;
     protected $calculateService;
     protected $summaryGradeRepository;
+    protected $courseSectionRepository;
 
     public function __construct(
         StudentRepositoryInterface $studentRepository,
         CalculateServiceInterface $calculateService,
-        SummaryGradeRepositoryInterface $summaryGradeRepository
+        SummaryGradeRepositoryInterface $summaryGradeRepository,
+        CourseSectionRepositoryInterface $courseSectionRepository,
     ) {
         $this->studentRepository = $studentRepository;
         $this->calculateService = $calculateService;
         $this->summaryGradeRepository = $summaryGradeRepository;
+        $this->courseSectionRepository = $courseSectionRepository;
     }
 
     //cập nhật điểm trung bình kiểm tra, điểm tổng kết, đánh giá
@@ -40,7 +45,6 @@ class SummaryGradeService implements SummaryGradeServiceInterface
             $student = $this->studentRepository->findOrFailById($studentId);
             //tính điểm trung bình kiểm tra
             $avgScore = $this->calculateService->calculateAverageExam($student, $courseSectionId);
-
             $summaryGrade = $this->summaryGradeRepository->updateOrCreate(['student_id' => $student->id, 'course_section_id' => $courseSectionId], ['avg_score' => $avgScore]);
             $finalScore = $this->calculateService->calculateFinalScore($summaryGrade);
             $summaryGrade->final_score = $finalScore;
@@ -56,7 +60,8 @@ class SummaryGradeService implements SummaryGradeServiceInterface
     public function updateSummaryGrades($courseSectionId)
     {
         try {
-            $students = $this->studentRepository->getStudentsAndGradesBycourseSectionId($courseSectionId);
+            $courseSection = $this->courseSectionRepository->findOrFailById($courseSectionId);
+            $students = $courseSection->students->where('status', StudentStatus::Active);
             foreach ($students as $student) {
                 $this->updateSummaryGrade($student->id, $courseSectionId);
             }
