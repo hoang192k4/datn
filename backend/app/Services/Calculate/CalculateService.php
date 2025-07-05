f91b4b02c7cdd44366b94f68b3a76091b29de028
DOng <khanhdong1702pro@gmail.com>
2025-07-05 17:29:38 +0700

    update cal attendacne

--- backend/app/Services/Calculate/CalculateService.php
+++ backend/app/Services/Calculate/CalculateService.php
@@ -57,7 +57,7 @@ class CalculateService implements CalculateServiceInterface
     {
         $totalAttendanceByCourseSection = $this->attendanceRepository->totalAttendanceStudentByCourseSection($studentId, $courseSectionId);
         $totalSessionByCourseSection = $this->attendanceRepository->totalSessionByCourseSection($courseSectionId);
-        $attendanceScore = 10 - ($totalSessionByCourseSection - $totalAttendanceByCourseSection);
+        $attendanceScore = 10 - max(0, ($totalSessionByCourseSection - $totalAttendanceByCourseSection));
         return $attendanceScore;
     }
 }
